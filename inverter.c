542def531cffe4ce4331627455c7a57a10f123bc
Davi Emmanuel <daviemmanuel37@gmail.com>
2026-08-18 17:42:19 -0300

    Add inverter program to swap two numbers

---
+++ inverter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+  
+int main(int argc, char *argv[]) {
+  int a, b;
+ 
+  printf("Digite o primeiro numero: ");
+scanf("%d", &a);
+
+printf("Digite o segundo numero: ");
+scanf("%d", &b);
+
+  printf("Esse numero invertendo fica: %d%d\n", b,a);
+  
+  return 0;
+}
